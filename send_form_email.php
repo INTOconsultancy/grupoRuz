9b372e7ce1a8b6b69a0ba8ab4a67a8df73be8ea8
rafabrutaldrums <rafael@gafa.mx>
2017-07-26 13:33:23 -0500

    ruz 2017

---
+++ send_form_email.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<!-- This site was created in Webflow. http://www.webflow.com-->
+<!-- Last Published: Mon May 23 2016 16:49:00 GMT+0000 (UTC) -->
+<html data-wf-site="5703f06e2408f8f35502e68c" data-wf-page="5730e6edfa657b1405d8bd9e">
+
+<head>
+    <meta charset="utf-8">
+    <title>Contacto</title>
+    <meta property="og:title" content="Contacto">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/webstyle.css">
+  <link rel="stylesheet" type="text/css" href="css/grupo-ruz.style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+    <script>
+        WebFont.load({
+            google: {
+                families: ["Alegreya Sans:regular", "Pacifico:regular", "Nunito:300,regular,700"]
+            }
+        });
+    </script>
+    <script type="text/javascript" src="js/modernizr.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon-32x32.png.png">
+    <link rel="apple-touch-icon" href="images/favicon-256x256.png">
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-80094409-1'], ['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script');
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;" />
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+  <link href="css/tipuesearch.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+  <script src="js/detect.js"></script>
+    </head>
+
+<body id="form-php">
+
+    <?php
+if(isset($_POST['email'])) {
+ 
+    // Edita las dos líneas siguientes con tu dirección de correo y asunto personalizados
+ 
+    $email_to = "servicioalcliente@gruporuz.com.mx";
+ 
+    $email_subject = "Contacto Grupo Ruz";   
+ 
+    function died($error) {
+ 
+        // si hay algún error, el formulario puede desplegar su mensaje de aviso
+ 
+        echo "Lo sentimos, hubo un error en sus datos y el formulario no puede ser enviado en este momento. ";
+ 
+        echo "Detalle de los errores.<br /><br />";
+        
+        echo $error."<br /><br />";
+ 
+        echo "Por favor modifique estos errores e inténtelo de nuevo.<br /><br />";
+        die();
+    }
+ 
+    // Se valida que los campos del formulairo estén llenos
+ 
+    if(!isset($_POST['first_name']) ||
+       
+    !isset($_POST['email']) || 
+       
+    !isset($_POST['asunto']) || 
+       
+    !isset($_POST['interes']) ||
+    
+    !isset($_POST['pais']) || 
+    
+    !isset($_POST['estado']) || 
+       
+    !isset($_POST['message'])) 
+    
+    {
+ 
+        died('Lo sentimos pero parece haber un problema con los datos enviados.');       
+ 
+    }
+
+ //En esta parte el valor "name" nos sirve para crear las variables que recolectaran la información de cada campo
+    
+    $first_name = $_POST['first_name']; // requerido
+ 
+    $email_from = $_POST['email']; // requerido
+    
+    $pais = $_POST['pais']; // requerido
+    
+    $estado = $_POST['estado']; // requerido
+ 
+    $telephone = $_POST['telephone']; // no requerido 
+
+    $asunto = $_POST['asunto']; // no requerido 
+
+    $interes = $_POST['interes']; // no requerido 
+
+    $message = $_POST['message']; // requerido
+ 
+    $error_message = "Error";
+
+//En esta parte se verifica que la dirección de correo sea válida 
+    
+   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+ 
+  }
+
+//En esta parte se validan las cadenas de texto
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'El formato del nombre no es válido<br />';
+ 
+  }
+ 
+ 
+  if(strlen($message) > 2) {
+ 
+    $error_message .= 'El formato del texto no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) < 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+//A partir de aqui se contruye el cuerpo del mensaje tal y como llegará al correo
+
+    $email_message = "Contenido del Mensaje.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Nombre: ".clean_string($first_name)."\n";
+ 
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Teléfono: ".clean_string($telephone)."\n";
+
+    $email_message .= "Asunto: ".clean_string($asunto)."\n";
+
+    $email_message .= "Interés: ".clean_string($interes)."\n";
+    
+    $email_message .= "País: ".clean_string($pais)."\n";
+    
+    $email_message .= "Estado: ".clean_string($estado)."\n";
+ 
+    $email_message .= "Mensaje: ".clean_string($message)."\n";
+  
+ 
+//Se crean los encabezados del correo
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+
+
+
+        <!-- incluye aqui tu propio mensaje de Éxito-->
+
+        <div class="mensaje-enviado">
+            <p>Gracias por contactar con nosotros.
+                <br>Nos pondremos en contacto contigo a la brevedad</p>
+
+            <a href="http://www.gruporuz.com.mx/contacto">
+                <button class="btn-contacto" type="button" style="font-size:20px;">Regresar</button>
+            </a>
+        </div>
+        <?php
+ 
+}
+ 
+?>
+</body>
+
+</html>
